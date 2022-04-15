0f899d00eec8659e81995901b1d20cd0de113157
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-16 07:14:13 +0800

    main.py

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
+from pages import pages
+
 import streamlit as st
-st.set_page_config(page_title="Taba Pares House", page_icon=":chart_with_upwards_trend:", layout= "wide")
+from streamlit_multipage import MultiPage
 
-# Custom imports 
-from multiapp import MultiPage
-from apps import week,month,quarter,year
-
-
-
-# Create an instance of the app 
 app = MultiPage()
-st.markdown("""
-# Taba Pares House Sale's Forecasting using LSTM
-[Fabook page](https://www.facebook.com/TABAPARESHOUSE).
-""")
+app.st = st
 
-Navigation_name = st.selectbox("Select Prediction", ("WEEK", "MONTH", "QUARTER", "YEAR"))
+for app_name, app_function in pages.items():
+    app.add_app(app_name, app_function)
 
-
-# Title of the main page
-st.title("Data :page_facing_up: ")
-
-# Add all your applications (pages) here
-app.add_app("WEEK",week.app)
-app.add_app("MONTH",month.app)
-app.add_app("QUARTER",quarter.app)
-app.add_app("YEAR",year.app)
-
-# The main app
 app.run()
