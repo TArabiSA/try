174f7b80a213065d878b8ac1dab5743a41a1c416
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-16 04:52:44 +0800

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,48 +1,40 @@
-"""Frameworks for running multiple Streamlit applications as a single app.
 """
+This file is the framework for generating multiple Streamlit applications 
+through an object oriented framework. 
+"""
+
+# Import necessary libraries 
 import streamlit as st
 
-class MultiApp:
-    """Framework for combining multiple streamlit applications.
-    Usage:
-        def foo():
-            st.title("Hello Foo")
-        def bar():
-            st.title("Hello Bar")
-        app = MultiApp()
-        app.add_app("Foo", foo)
-        app.add_app("Bar", bar)
-        app.run()
-    It is also possible keep each application in a separate file.
-        import foo
-        import bar
-        app = MultiApp()
-        app.add_app("Foo", foo.app)
-        app.add_app("Bar", bar.app)
-        app.run()
-    """
-    def __init__(self):
-        self.apps = []
+# Define the multipage class to manage the multiple apps in our program 
+class MultiPage: 
+    """Framework for combining multiple streamlit applications."""
 
-    def add_app(self, title, func):
-        """Adds a new application.
-        Parameters
-        ----------
-        func:
-            the python function to render this app.
-        title:
-            title of the app. Appears in the dropdown in the sidebar.
+    def __init__(self) -> None:
+        """Constructor class to generate a list which will store all our applications as an instance variable."""
+        self.pages = []
+    
+    def add_page(self, title, func) -> None: 
+        """Class Method to Add pages to the project
+        Args:
+            title ([str]): The title of page which we are adding to the list of apps 
+            
+            func: Python function to render this page in Streamlit
         """
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
+
+        self.pages.append({
+          
+                "title": title, 
+                "function": func
+            })
 
     def run(self):
-        # app = st.sidebar.radio(
-        app = st.selectbox(
-            'Navigation',
-            self.apps,
-            format_func=lambda app: app['title'])
+        # Drodown to select the page to run  
+        page = st.sidebar.selectbox(
+            'App Navigation', 
+            self.pages, 
+            format_func=lambda page: page['title']
+        )
 
-        app['function']()
+        # run the app function 
+        page['function']()
