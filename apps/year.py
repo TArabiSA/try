de1371a39b68daf4b0bd56fea8ed0361f7736fa1
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-16 05:13:30 +0800

    Update year.py

--- apps/year.py
+++ apps/year.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 from streamlit_lottie import st_lottie
 import tensorflow as tf
-import math
+
 
 
 
