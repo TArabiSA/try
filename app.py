5cef3d2233be5707e849aa640713e8658d5f6ca1
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-16 04:57:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import data_upload, machine_learning, metadata, data_visualize, redundant, inference # import your pages here
+from apps import week, month, quarter, year # import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
@@ -11,11 +11,10 @@ app = MultiPage()
 st.title("Data Storyteller Application")
 
 # Add all your applications (pages) here
-app.add_page("Upload Data", data_upload.app)
-app.add_page("Change Metadata", metadata.app)
-app.add_page("Machine Learning", machine_learning.app)
-app.add_page("Data Analysis",data_visualize.app)
-app.add_page("Y-Parameter Optimization",redundant.app)
+app.add_page("Upload Data", week.app)
+app.add_page("Change Metadata", month.app)
+app.add_page("Machine Learning", quarter.app)
+app.add_page("Data Analysis", year.app)
 
 # The main app
 app.run()
