c22ca7b3043f21685b8e10562f6d3d0c460ff10f
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-16 05:16:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 # Custom imports 
 from multiapp import MultiPage
-import apps.week as app
-import apps 
+import apps as week
+import apps as month
 import apps as quarter
 import apps as year
 
@@ -16,10 +16,10 @@ app = MultiPage()
 st.title("Data Storyteller Application")
 
 # Add all your applications (pages) here
-app.add_page("Upload Data", week.app)
-app.add_page("Change Metadata", month.app)
-app.add_page("Machine Learning", quarter.app)
-app.add_page("Data Analysis", year.app)
+app.add_page("Upload Data", week)
+app.add_page("Change Metadata", month)
+app.add_page("Machine Learning", quarter)
+app.add_page("Data Analysis", year)
 
 # The main app
 app.run()
